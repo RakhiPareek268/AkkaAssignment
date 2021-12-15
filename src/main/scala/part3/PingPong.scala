430e911bff4fbd97db3a9b38d954349037bb295c
RakhiPareek268 <87005355+RakhiPareek268@users.noreply.github.com>
2021-12-15 21:43:30 +0530

    Update PingPong.scala

--- src/main/scala/part3/PingPong.scala
+++ src/main/scala/part3/PingPong.scala
@@ -18,16 +18,16 @@ object PingPong extends App {
     def increment{sum+=1}
     val pong = context.actorOf(Props[PongActor], "pong")
     override def receive: Receive = {
-      case PongMessage => increment; //sender ! PingMessage
+      case PongMessage => increment; 
       case Message => for {
         _ <- 1 to 10000
-      } yield pong ! PingMessage
+      } yield pong ! PingMessage                                                         //Send 10000 "ping" messages                       
       case End(s) => println(s"sum:$s")
-       pong ! GetPongSum(None)
-      case GetPongSum(s)=>println(s)
-      sender ! ThrowException()
-      sender ! GetPongSum(None)
-      sender ! GetPongSum(Some(sum))
+      pong ! GetPongSum(None)                                                            //Send from Ping to Pong a GetPongSum(None)
+      case GetPongSum(s)=>println(s)                                                     //In ping print the sum (should be 10000)
+      sender ! ThrowException()                                                          //Send ThrowException() From Ping to Pong
+      sender ! GetPongSum(None)                                                          //From Ping to Pong send GetPongSum(None) again
+      sender ! GetPongSum(Some(sum))                                                     // we got sum 9999
     }
   }
   class PongActor extends Actor with ActorLogging {
@@ -39,16 +39,16 @@ object PingPong extends App {
       case End(sum)=>println(s"counter:$sum")
       case PingMessage=>
         val newSum=Future{
-        sum+=doWork()
+        sum+=doWork()                                                           // wrap do work and result of sum in Future
       }
         Await.result(newSum,Duration.Inf)
         if (sum<10000) ping ! PongMessage
         else if(sum==10000) {
           sender ! End(sum)
-          sender ! GetPongSum(Some(sum))
+          sender ! GetPongSum(Some(sum))                                       //In Pong send sum in GetPongSum
         }
       case GetPongSum(n)=>println(n)
-      case ThrowException() =>println(Try {throw new Exception()})
+      case ThrowException() =>println(Try {throw new Exception()})            //In Pong throw an exception (throw new Exception())
     }
   }
   ping ! Message
