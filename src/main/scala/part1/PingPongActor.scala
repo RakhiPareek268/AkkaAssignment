aaadd21fc1d56bbb18b5d96579d12fb6307782cd
RakhiPareek268 <87005355+RakhiPareek268@users.noreply.github.com>
2021-12-15 21:28:02 +0530

    Update PingPongActor.scala

--- src/main/scala/part1/PingPongActor.scala
+++ src/main/scala/part1/PingPongActor.scala
@@ -4,10 +4,10 @@ import java.time.Clock.system
 import scala.sys.props
 object PingPongActor extends App {
   val actorSystem=ActorSystem("PingPong")                       //creating actor system
-  val ping=actorSystem.actorOf(Props[PingActor],"ping")  // creating instance of ping actor
+  val ping=actorSystem.actorOf(Props[PingActor],"ping")         // creating instance of ping actor
   case class Message(message:String)
   class PingActor extends Actor with ActorLogging {
-    val pong = context.actorOf(Props[PongActor], "pong") // create pong as a child of ping actor
+    val pong = context.actorOf(Props[PongActor], "pong")       // create pong as a child of ping actor
     override def receive: Receive = {
       case message:String=>log.info{message}                   // using log.info we are printing the message send by pong actor as acknowledgement
       case Message=> pong ! "ping"
